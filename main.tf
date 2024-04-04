0cf96e301252af083fce46f991e1d80fb0a74852
BETRAND MUTAGHA <betrandmutagha@BETRANDs-MBP.lan>
2024-04-04 14:09:08 -0400

    authenticate to azure

--- main.tf
+++ main.tf
@@ -1,41 +1,20 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup"
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "myVNet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
+resource "azurerm_storage_account" "example" {
+  name                     = "mystorageaccount"  # Name of your storage account
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "mySubnet"
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# Add other resources (e.g., public IP, network security group, etc.) as needed
-
-resource "azurerm_windows_virtual_machine" "myvm" {
-  name                  = "myVM"
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = azurerm_resource_group.rg.location
-  size                  = "Standard_B2s"
-  admin_username        = "myadmin"
-  admin_password        = "SuperSecretPassword123!"
-  network_interface_ids = [azurerm_network_interface.myvm_nic.id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
+  tags = {
+    environment = "dev"
   }
 }
