c58a7e09f0c4d50c48efbd168180752539a622f1
BETRAND MUTAGHA <betrandmutagha@BETRANDs-MBP.lan>
2024-04-04 14:17:50 -0400

    authenticate to azure

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "mystorageaccount"  # Name of your storage account
+  name                     = "mystorageaccount123"  # Unique name for the storage account
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
