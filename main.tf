7a209bad21fcdb7c7cb008e9e2b6a56765b3637d
BETRAND MUTAGHA <betrandmutagha@BETRANDs-MBP.lan>
2024-04-04 14:42:28 -0400

    authenticate to azure

--- main.tf
+++ main.tf
@@ -3,18 +3,17 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"  # Update with your desired location
+  name     = "myResourceGroup"
+  location = "East US"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "mystorageaccount"  # Update with your desired storage account name
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
+  name                = "storageaccountname"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  account_tier        = "Standard"
   account_replication_type = "LRS"
-
   tags = {
-    environment = "example"
+    environment = "staging"
   }
 }
